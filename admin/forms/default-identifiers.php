df25196c0980376762c89eb3a014a4411641f6fc
tomm-tomm <tomas.hlavac@pm.me>
2024-12-16 13:17:29 +0100

    club settings commented

--- admin/forms/default-identifiers.php
+++ admin/forms/default-identifiers.php
@@ -1,11 +1,14 @@
 <?php
 /*
+ *
+ * NOTE: This section doesn't work noe because chess.sk changed HTML structure.
+ *
  * For "default -> identifiers" page in admin theme settings
  * Loaded in index.php file
  *
  * 1. Db operations
  * 2. Form
- * 
+ *
  * @package Chess_Scraper
  * @version 1.0.0
  */
@@ -23,9 +26,9 @@ $db_table = $wpdb->prefix . 'chess_scraper_team_settings';
 // NOTE:
 // Column name => column type
 // s = string, d = integer, f = float
-$db_cols = array( 'team_id' => 'd',
+$db_cols = array( 'team_id'       => 'd',
                   'federation_id' => 's',
-                  'changed_at' => 's' );
+                  'changed_at'    => 's' );
 
 // Do db operations (update table/select data)
 $query_values = form_ops( $db_table, $db_cols );
@@ -39,9 +42,11 @@ $query_values = form_ops( $db_table, $db_cols );
 chsc_admin_form_start( $page, $section );
 
 chsc_admin_form_block_start( 'Club identifiers (for Slovakia federation only now)' );
-chsc_admin_form_note( 'Fill in both identifiers for the club whose data you want to upload to the database. Then submit the form.<br>Subsequent uploading of data may take several tens of seconds. Please wait until the end to upload the data correctly.' );
+chsc_admin_form_note( '<div style="color: #ff0000; font-weight: bold;">This section doesn\'t work now.</div>' );
+//chsc_admin_form_note( 'Fill in both identifiers for the club whose data you want to upload to the database. Then submit the form.<br>Subsequent uploading of data may take several tens of seconds. Please wait until the end to upload the data correctly.' );
 
 /** Club #1 **/
+/*
 $this_team_id = ( int )$query_values[ 'team_id_1' ][ 0 ];
 $this_federation_id = $query_values[ 'federation_id_1' ][ 0 ];
 
@@ -63,7 +68,7 @@ if ( $this_federation_id != '' &&
 chsc_admin_form_block_end();
 
 // Submit button
-chsc_admin_form_submit( 'Submit club settings' );
+chsc_admin_form_submit( 'Submit club settings' );*/
 
 // End form
 chsc_admin_form_end();
